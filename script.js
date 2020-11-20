7a14993116a9aeed46c2ce9d28c85021e611e771
Paul Rogers <progers999@hotmail.co.uk>
2020-11-20 14:59:35 +0000

    Fetch API added

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Hello World!')
+let loc = 'wellington'
+
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=1defcd4c96f2a8a98157d00156e7fe7e`)
+	.then(response => response.json())
+	.then(data => {
+		console.log(data)
+	})
