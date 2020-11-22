a3f31eacb9d6b525e55700236f8378ef06fab3fe
Paul Rogers <progers999@hotmail.co.uk>
2020-11-22 15:41:49 +0000

    Styles refined

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let loc = 'wellington'
+let loc = 'cwmbran'
 
 fetch(`http://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=1defcd4c96f2a8a98157d00156e7fe7e`)
 	.then(response => response.json())
