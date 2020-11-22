44d111b03a198034502411c5cf4c2e33a53f6ea8
Paul Rogers <progers999@hotmail.co.uk>
2020-11-22 15:54:19 +0000

    Searching for a city or town now logs the API data to the console

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-let loc = 'cwmbran'
+const form = document.getElementById('form');
+const search = document.getElementById('search');
 
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=1defcd4c96f2a8a98157d00156e7fe7e`)
-	.then(response => response.json())
-	.then(data => {
-		console.log(data)
-	})
+const getWeather = (loc) => {
+	fetch(`http://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=1defcd4c96f2a8a98157d00156e7fe7e`)
+		.then(response => response.json())
+		.then(data => {
+			console.log(data)
+		})
+}
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	let loc = search.value
+	getWeather(loc);
+
+	search.value = '';
+})
