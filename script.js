ec221c6c99321c2518b3e44506249606a90f1b1c
Paul Rogers <progers999@hotmail.co.uk>
2020-11-30 12:32:49 +0000

    Fixed an issue with retrieving api data

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ const clearElement = () => {
 };
 
 const getWeather = (loc) => {
-	fetch(`http://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=1defcd4c96f2a8a98157d00156e7fe7e`)
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=1defcd4c96f2a8a98157d00156e7fe7e`)
 		.then(response => response.json())
 		.then(data => {
 			search.classList.remove('error');
